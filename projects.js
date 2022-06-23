99c0567ddd78551e06e3706d9cddff9e869343ef
hislordshipprof <proflord63@gmail.com>
2022-06-23 08:27:54 +0000

    Final capstone with various pages

---
+++ projects.js
@@ -0,0 +1,46 @@
+const projectsSection = document.querySelector('.featured-wrapper');
+// const wrapper = document.getElementsByClassName('feature-wrapper');
+
+const features = [
+  {
+    name: 'Web Development',
+    subText: 'Wondering how to get started as a Beginner?',
+    mainText: 'We got you covered ranging from the best tutuorial out there. For better roadmap on how to get started in the field of web developmentwe got you covered!!',
+    image: 'app-dev',
+  },
+  {
+    name: 'JavaScript Tutotrial',
+    subText: 'Wondering how to get started as a Beginner?',
+    mainText: 'We got you covered ranging from the best tutuorial out there. For better roadmap on how to get started in the field of web developmentwe got you covered!!',
+    image: 'js',
+  },
+  {
+    name: 'Python Programming',
+    subText: 'Wondering how to get Started with Python Programming?',
+    mainText: 'We got you covered ranging from the best tutuorial out there. For better roadmap on how to get started in the field of web developmentwe got you covered!!',
+    image: 'py',
+  },
+  {
+    name: 'HTML 5/CSS Tutorials',
+    subText: 'Wondering how to get started as a Beginner?',
+    mainText: 'We got you covered ranging from the best tutuorial out there. For better roadmap on how to get started in the field of web developmentwe got you covered!!',
+    image: 'html',
+  },
+];
+
+function CreateCourse() {
+  for (let i = 0; i < features.length; i += 1) {
+    projectsSection.innerHTML += `  <div class="featured-card">
+      <img src="/img/${features[i].image}.png" alt="images" class="featured-image" >
+     
+      <div class="abc">
+         <h1 class="xyz">${features[i].name}</h1>
+      <p class="main-text">${features[i].subText}</p>
+      <p class="sub-text">${features[i].mainText}</p>
+     </div> 
+    </div>
+ `;
+
+  }
+}
+CreateCourse()
